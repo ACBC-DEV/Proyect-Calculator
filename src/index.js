c6f11f0345ea8feaf68ce1c18568e73de601c65b
Camilo Blandon <camiloblandon69@gmail.com>
2023-06-15 13:29:13 -0500

    end of the proyect in v-1.0

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function addToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+function calculate() {
+  const displayValue = document.getElementById('display').value;
+  const result = eval(displayValue);
+  document.getElementById('display').value = result;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
